a15e24d6904c68085299c1fef382f013587939ef
Tolga Eren <tolga.eren_1@philips.com>
2020-08-05 14:14:57 +0200

    removed entrypoint

--- generate_strategy_matrix.py
+++ generate_strategy_matrix.py
@@ -5,6 +5,7 @@ import json
 import pathlib
 import re
 import os
+from string import Template
 
 root = pathlib.Path(__file__).parent.resolve()
 client = GraphqlClient(endpoint="https://api.github.com/graphql")
@@ -14,26 +15,21 @@ TOKEN = os.environ.get("SIMONW_TOKEN", "")
 
 
 def make_query(owner, name, last_n):
-    return (
+    return Template(
         """
-query { 
-  repository(owner:"$OWNER", name: "$NAME") { 
-    releases(last: $LAST_N){
-      nodes{
-        tagName
-      }
-    }
-  }
-}
-""".replace(
-            "$OWNER", owner
-        )
-        .replace("$NAME", name)
-        .replace("$LAST_N", str(last_n))
-    )
+        query { 
+        repository(owner:"$owner", name: "$name") { 
+            releases(last: $last_n){
+            nodes{
+                tagName
+            }
+            }
+        }
+        }
+        """
+    ).substitute(name=name, owner=owner, last_n=str(last_n))
 
-
-def fetch_releases(token, owner, name, last_n:int):
+def fetch_releases(token, owner, name, last_n: int):
     releases = []
     data = client.execute(
         query=make_query(owner, name, 100),
@@ -48,13 +44,16 @@ def fetch_releases(token, owner, name, last_n:int):
     return releases
 
 
-def strategy_matrix(token, owner, name, last_n:int, python_versions, tag_latest=True):
+def strategy_matrix(token, owner, name, last_n: int, python_versions, tag_latest=True):
     repo_versions = fetch_releases(token=token, owner=owner, name=name, last_n=last_n)
     repo_name_for_matrix = f"{args.name.upper()}_VERSION"
     matrix = {
         "include": [
-            {repo_name_for_matrix: repo_version, "PYTHON_VERSION": python_version,
-            "TAGS":f"{python_version}-{repo_version}"}
+            {
+                repo_name_for_matrix: repo_version,
+                "PYTHON_VERSION": python_version,
+                "TAGS": f"{python_version}-{repo_version}",
+            }
             for python_version in python_versions
             for repo_version in repo_versions
         ]
@@ -78,13 +77,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--token", help="Github personal access token", type=str, required=True,
     )
-    
+
+    parser.add_argument(
+        "--python_versions", help='List of python versions for the base image. i.e. "3.8,3.7,3.6"', type=str, default="3.8,3.7,3.6",
+    )
+
     args = parser.parse_args()
-    
-    pythons = [3.8, 3.7, 3.6]
+    args.python_versions = args.python_versions.split(",")
 
-    matrix = strategy_matrix(python_versions=pythons, **vars(args))
+    matrix = strategy_matrix(**vars(args))
     print(json.dumps(matrix))
-
-    
 
